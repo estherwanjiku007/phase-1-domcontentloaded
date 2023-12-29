f988ff82467ad7ac581d6f6657162c0790a28cb4
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-29 17:33:04 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(targetAttribute){
+    alert("This document has been loaded.")
+})
+function targetAttribute(){
+    document.getElementById('text').textContent="This is really cool";
+}    
+
